b29409ea93bc7ebbb688d1c9596e990f9d9c1f90
Sensus Media <36053809+sensusmedia@users.noreply.github.com>
2018-12-13 11:47:22 -0700

    Create scroll-to-top.js

---
+++ scroll-to-top.js
@@ -0,0 +1,5 @@
+<script type="text/javascript">
+     jQuery(document).on('click','#arrow-up',function(){
+                jQuery('html,body').animate({scrollTop: jQuery('#page').offset().top},'slow');
+            })
+</script>
